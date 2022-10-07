a98e1ae03991d9522d22f8b2e71c607136147a30
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-07 11:50:49 +0900

    creatAFile 메서드 추가

--- src/week3/day4/PopulationStatistics.java
+++ src/week3/day4/PopulationStatistics.java
@@ -1,25 +1,17 @@
 package week3.day4;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 // fileReader.read()는 한 바이트씩 읽는 것 --> 모래를 한 알씩 옮기는 것으로 오래걸림
 public class PopulationStatistics {
-    public static void main(String[] args) throws IOException {
-
-        String address = "/Users/jinhyuck/Downloads/2021_인구관련연간자료_20221007_89221.csv";
-        BufferedReader reader = new BufferedReader(new FileReader(address));
-
-        char ch = (char) reader.read();
-        System.out.println(ch);
-//        String fileContents = "";
-//        readByChar(address);
-
-    }
 
     public void readByChar(String fileName) throws IOException {
         FileReader reader = new FileReader(fileName);
@@ -31,15 +23,18 @@ public class PopulationStatistics {
         reader.close();
     }
 
-    public void readByLine(String fileName) throws  IOException{
+    public List<PopulationMove> readByLine(String fileName) throws  IOException{
+        List<PopulationMove> pml = new ArrayList<>();
         BufferedReader reader = new BufferedReader(
-                new FileReader("d:\\file.txt")
+                new FileReader(fileName)
         );
         String fileContents;
         while ((fileContents = reader.readLine()) != null) {
-            System.out.println(fileContents);
+//            System.out.println(fileContents);
+            PopulationMove pm = parse(fileName);
         }
         reader.close();
+        return pml;
     }
 
 
@@ -55,6 +50,22 @@ public class PopulationStatistics {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public PopulationMove parse(String data) {
+
+        String[] arr = data.split(",");
+        return new PopulationMove(arr[0], arr[6]);
+    }
+
+    public void createAFile(String fileName) {
+        File file = new File(fileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
     }
 
 }
