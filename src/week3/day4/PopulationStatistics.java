90a7194fbcbd089b0a96a01ff45e44e55a4be6ce
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-07 13:42:14 +0900

    파일저장 및 불러오기

--- src/week3/day4/PopulationStatistics.java
+++ src/week3/day4/PopulationStatistics.java
@@ -83,16 +83,17 @@ public class PopulationStatistics {
 
     public static void main(String[] args) throws IOException {
         String address = "/Users/jinhyuck/Downloads/2021_인구관련연간자료_20221007_89221.csv";
+        String addressNew = "./from_to.txt";
         PopulationStatistics populationStatistics = new PopulationStatistics();
-        List<PopulationMove> pml = populationStatistics.readByLine(address);
+        List<PopulationMove> pml = populationStatistics.readByLine(addressNew);
 
         // 파일저장
 //        populationStatistics.createAFile("/from_to.txt");
         List<String> strings = new ArrayList<>();
         for (PopulationMove pm : pml) {
-            String fromTo = populationStatistics.fromToString(pm);
-            strings.add(fromTo);
-//            System.out.printf("전입: %s, 전출: %s \n", pm.getFromSido(), pm.getToSido());
+//            String fromTo = populationStatistics.fromToString(pm);
+//            strings.add(fromTo);
+            System.out.printf("전입: %s, 전출: %s \n", pm.getFromSido(), pm.getToSido());
         }
 //        System.out.println(pml.size());
         populationStatistics.write(strings, "./from_to.txt");
