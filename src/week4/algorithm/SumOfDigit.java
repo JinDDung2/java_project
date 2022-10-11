d05f55c3cb39c9c80f300806665eeb3030803574
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-11 09:42:00 +0900

    자리수 더하기(연산자 활용 추가)

--- src/week4/algorithm/SumOfDigit.java
+++ src/week4/algorithm/SumOfDigit.java
@@ -3,7 +3,7 @@ package week4.algorithm;
 import java.util.Scanner;
 
 public class SumOfDigit {
-    public int solution(String strNum) {
+    public int solutionA(String strNum) {
         int result = 0;
         for (int i = 0; i < strNum.length(); i++) {
             result += Integer.parseInt(String.valueOf(strNum.charAt(i)));
@@ -11,14 +11,33 @@ public class SumOfDigit {
         return result;
     }
 
+    // 몫과 나머지 연산자를 활용해보기
+    public int solutionB(int num) {
+        int result = 0;
+
+        // for문이 아니라 while 사용
+        while (num > 0) {
+
+            // 나머지를 먼저 구해야한다
+            result += num % 10;
+            num /= 10;
+            System.out.printf("result: %d, num: %d \n", result, num);
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String strNum = sc.nextLine();
+        SumOfDigit sod = new SumOfDigit();
 
-        SumOfDigit solution = new SumOfDigit();
-        int result = solution.solution(strNum);
-        System.out.println(result);
+//        String strNum = sc.nextLine();
+        /*int resultA = sod.solutionA(strNum);
+        System.out.println(resultA);*/
 
+        int num = sc.nextInt();
+        int resultB = sod.solutionB(num);
+        System.out.println(resultB);
 
     }
 }
