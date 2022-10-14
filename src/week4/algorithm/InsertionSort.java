16b875b09ead999433fac60e50c746de3a654218
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-14 10:38:21 +0900

    삽입정렬 재귀 구현

--- src/week4/algorithm/InsertionSort.java
+++ src/week4/algorithm/InsertionSort.java
@@ -33,12 +33,27 @@ public class InsertionSort {
         return arr;
     }
 
+    public int[] sortV3(int[] arr, int i) {
+        if (i == arr.length) return arr;
+
+        for (int j=i-1; j>=0; j--) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+
+        return sortV3(arr, i+1);
+    }
+
     public static void main(String[] args) {
         // 첫번째 인덱스엥서 출발
         int[] arr = {8, 5, 6, 2, 4};
         InsertionSort sortArr = new InsertionSort();
 //        int[] result = sortArr.sortV1(arr, 2);
-        int[] result = sortArr.sortV2(arr);
+//        int[] result = sortArr.sortV2(arr);
+        int[] result = sortArr.sortV3(arr, 1);
         System.out.println(Arrays.toString(result));
 
     }
