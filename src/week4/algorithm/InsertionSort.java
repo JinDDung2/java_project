f0d30bd6eb627347cd31d5479f783fde68fe8b53
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-14 10:25:45 +0900

    삽입정렬 구현

--- src/week4/algorithm/InsertionSort.java
+++ src/week4/algorithm/InsertionSort.java
@@ -4,12 +4,30 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-    public int[] sort(int[] arr) {
-        if (arr[1] < arr[0]) {
-            int temp = 0;
-            temp = arr[0];
-            arr[0] = arr[1];
-            arr[1] = temp;
+    public int[] sortV1(int[] arr, int i) {
+        for (int j=i; j>0; j--) {
+//            System.out.printf("i:%d, j:%d, j-1:%d\n", i, j, j-1);
+            if (arr[j] < arr[j-1]) {
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        return arr;
+    }
+
+    public int[] sortV2(int[] arr) {
+        for (int i=1; i<arr.length; i++) {
+            for (int j=i; j>0; j--) {
+    //            System.out.printf("i:%d, j:%d, j-1:%d\n", i, j, j-1);
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
         }
 
         return arr;
@@ -19,8 +37,8 @@ public class InsertionSort {
         // 첫번째 인덱스엥서 출발
         int[] arr = {8, 5, 6, 2, 4};
         InsertionSort sortArr = new InsertionSort();
-        int[] result = sortArr.sort(arr);
-
+//        int[] result = sortArr.sortV1(arr, 2);
+        int[] result = sortArr.sortV2(arr);
         System.out.println(Arrays.toString(result));
 
     }
