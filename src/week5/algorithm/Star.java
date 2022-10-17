cfeac20bd3f45b0cf29ef6e52afeb9a52f1157dd
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-17 09:10:43 +0900

    별 찍기

---
+++ src/week5/algorithm/Star.java
@@ -0,0 +1,17 @@
+package week5.algorithm;
+
+public class Star {
+
+    public void rightTriangle() {
+        System.out.println("* ");
+        System.out.println("* * ");
+        System.out.println("* * * ");
+        System.out.println("* * * *");
+    }
+
+    public static void main(String[] args) {
+        Star star = new Star();
+
+        star.rightTriangle();
+    }
+}
