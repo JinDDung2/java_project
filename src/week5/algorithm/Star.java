5956492f4ffb97fea40e542669cc37cdfdb7592d
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-17 09:21:20 +0900

    별 찍기 (for문 사용)

--- src/week5/algorithm/Star.java
+++ src/week5/algorithm/Star.java
@@ -2,16 +2,26 @@ package week5.algorithm;
 
 public class Star {
 
-    public void rightTriangle() {
+    public void rightTriangleV1() {
         System.out.println("* ");
         System.out.println("* * ");
         System.out.println("* * * ");
         System.out.println("* * * *");
     }
 
+    public void rightTriangleV2(int num) {
+        for (int i=0; i<num; i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Star star = new Star();
-
-        star.rightTriangle();
+        int num = 4;
+//        star.rightTriangleV1();
+        star.rightTriangleV2(num);
     }
 }
