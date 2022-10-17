0f74d243e0873d5b59d1630a294a577744fdad47
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-17 09:40:17 +0900

    피라미드 만들기

--- src/week5/algorithm/Star.java
+++ src/week5/algorithm/Star.java
@@ -25,12 +25,25 @@ public class Star {
         }
     }
 
+    public void pyramid(int num) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 1; j < num-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print(str);
+            }
+            System.out.println();
+        }
+    }
+
 
 
     public static void main(String[] args) {
-        Star star = new Star("#");
+        Star star = new Star();
         int num = 4;
 //        star.rightTriangleV1();
-        star.rightTriangleV2(num);
+//        star.rightTriangleV2(num);
+        star.pyramid(num);
     }
 }
