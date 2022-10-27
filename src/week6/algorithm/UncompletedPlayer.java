897afb8f4297d0059be702f9199d3efe59035fb4
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-27 09:25:49 +0900

    완주하지 못한 선수

--- src/week6/algorithm/UncompletedPlayer.java
+++ src/week6/algorithm/UncompletedPlayer.java
@@ -20,11 +20,32 @@ public class UncompletedPlayer {
         return players.get(num);
     }
 
+    public String solutionV2(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> players = new HashMap<>();
+        for (String player : participant) {
+            players.put(player, players.getOrDefault(player, 0)+1);
+        }
+
+        for (String completed : completion) {
+            players.put(completed, players.get(completed) - 1);
+        }
+
+        for (String key : players.keySet()) {
+            if (players.get(key) != 0) {
+                answer = key;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         UncompletedPlayer sol = new UncompletedPlayer();
         String[] participant = new String[]{"mislav", "stanko", "mislav", "ana"};
         String[] completion = new String[]{"stanko", "ana", "mislav"};
         String answer = sol.solution(participant, completion);
+        String answer2 = sol.solutionV2(participant, completion);
         System.out.println(answer);
+        System.out.println(answer2);
     }
 }
