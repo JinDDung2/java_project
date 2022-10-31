7eacb0c161628bc7b4aa7a56d7d720c24daf4cd4
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-31 09:51:36 +0900

    모의고사 for문 수정

--- src/week7/algorithm/MockTest.java
+++ src/week7/algorithm/MockTest.java
@@ -9,16 +9,18 @@ public class MockTest {
     public ArrayList<Integer> solution(int[] answers) {
         int[] scores = new int[3];
         int[] firstArr = {1, 2, 3, 4, 5};
-        int[] secondArr = {2, 1, 2, 2, 2, 3, 2, 4, 2, 5};
+        int[] secondArr = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] thirdArr = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
         // 정답일치하는지
         for (int i = 0; i < answers.length; i++) {
             if (firstArr[i % firstArr.length] == answers[i]) {
                 scores[0] += 1;
-            } else if (secondArr[i % secondArr.length] == answers[i]) {
+            }
+            if (secondArr[i % secondArr.length] == answers[i]) {
                 scores[1] += 1;
-            } else if (thirdArr[i % thirdArr.length] == answers[i]) {
+            }
+            if (thirdArr[i % thirdArr.length] == answers[i]) {
                 scores[2] += 1;
             }
         }
@@ -31,17 +33,17 @@ public class MockTest {
                 max = score;
             }
         }
-        System.out.println("max = " + max);
+        // System.out.println("max = " + max);
 
         // 배열크기
         for (int i = 0; i < 3; i++) {
             if (scores[i] == max) {
-                list.add(i + 1);
+                list.add(i+1);
             }
         }
 
-        System.out.println(list);
-        System.out.println(Arrays.toString(scores));
+        // System.out.println(Arrays.toString(scores));
+        // System.out.println(list);
         return list;
     }
     public static void main(String[] args) {
