8d3c53881ce2e9a10c2e262ab3e772ed72995fd0
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-01 09:28:24 +0900

    [add] 소수판별 메서드V1

---
+++ src/week7/algorithm/Prime.java
@@ -0,0 +1,36 @@
+package week7.algorithm;
+
+public class Prime {
+    // 소수인 경우의 수 찾기
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i < n+1; i++) {
+            if (isPrimeV1(n) == true) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+
+    // 소수인지 판별하기 1번
+    public boolean isPrimeV1(int n) {
+        if (n == 0 || n == 1) return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+        Prime prime = new Prime();
+        int result = prime.solution(10);
+        System.out.println("result = " + result);
+        boolean primeV1 = prime.isPrimeV1(5);
+        System.out.println("primeV1 = " + primeV1);
+    }
+}
