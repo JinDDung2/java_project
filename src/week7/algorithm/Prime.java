27aabef2466d10d08cb1b9c2e2f6fb830ae40b84
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-01 09:28:49 +0900

    [add] 소수판별 메서드V2

--- src/week7/algorithm/Prime.java
+++ src/week7/algorithm/Prime.java
@@ -24,6 +24,17 @@ public class Prime {
         return true;
     }
 
+    // 소수인지 판별하기 2번
+    public boolean isPrimeV2(int n) {
+        if (n == 0 || n == 1) return false;
+
+        for (int i = 2; i < n/2+1; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
 
 
     public static void main(String[] args) {
