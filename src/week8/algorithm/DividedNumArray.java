683fc2a5a76a4b856db5a03af4d0b67216a2d17d
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-07 09:49:25 +0900

    [add] 우선순위 큐 오름차순 고민

--- src/week8/algorithm/DividedNumArray.java
+++ src/week8/algorithm/DividedNumArray.java
@@ -1,8 +1,8 @@
 package week8.algorithm;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class DividedNumArray {
     public static ArrayList<Integer> solution(int[] arr, int divisor) {
@@ -23,9 +23,30 @@ public class DividedNumArray {
         return answer;
     }
 
+    // [1, 3, 2, 36] 정렬이 안되어있다..
+    public static PriorityQueue<Integer> solutionV2(int[] arr, int divisor) {
+        PriorityQueue<Integer> answer = new PriorityQueue<>();
+
+        for (int num : arr) {
+            if (num % divisor == 0) {
+                answer.add(num);
+            }
+        }
+
+        if (answer.size() == 0) {
+            answer.add(-1);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         System.out.println(solution(new int[]{5, 9, 7, 10}, 5));
         System.out.println(solution(new int[]{2, 36, 1, 3}, 1));
         System.out.println(solution(new int[]{3, 2, 6}, 10));
+        System.out.println("----------------------------");
+        System.out.println(solutionV2(new int[]{5, 9, 7, 10}, 5));
+        System.out.println(solutionV2(new int[]{2, 36, 1, 3}, 1));
+        System.out.println(solutionV2(new int[]{3, 2, 6}, 10));
     }
 }
