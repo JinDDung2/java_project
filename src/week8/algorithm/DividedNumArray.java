eb818737a822c0a819e13b6c90bd527b554bcfea
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-07 10:27:38 +0900

    [update] 우선순위 큐 활용

--- src/week8/algorithm/DividedNumArray.java
+++ src/week8/algorithm/DividedNumArray.java
@@ -1,6 +1,7 @@
 package week8.algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
@@ -24,19 +25,24 @@ public class DividedNumArray {
     }
 
     // [1, 3, 2, 36] 정렬이 안되어있다..
-    public static PriorityQueue<Integer> solutionV2(int[] arr, int divisor) {
-        PriorityQueue<Integer> answer = new PriorityQueue<>();
+    public static int[] solutionV2(int[] arr, int divisor) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int num : arr) {
             if (num % divisor == 0) {
-                answer.add(num);
+                pq.offer(num);
             }
         }
 
-        if (answer.size() == 0) {
-            answer.add(-1);
+        if (pq.isEmpty()) return new int[]{-1};
+
+        int[] answer = new int[pq.size()];
+        int idx = 0;
+        while (!pq.isEmpty()) {
+            answer[idx++] = pq.poll();
         }
 
+//        System.out.println("answer = " + Arrays.toString(answer));
         return answer;
     }
 
