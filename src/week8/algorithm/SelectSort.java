5ab7b5f05b57e18b79ebf19aea250047b9d69315
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-09 09:34:51 +0900

    [update] selectSort 구현

--- src/week8/algorithm/SelectSort.java
+++ src/week8/algorithm/SelectSort.java
@@ -5,13 +5,23 @@ import java.util.Arrays;
 public class SelectSort {
     public static void main(String[] args) {
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-        int min = arr[0];
-        // i=0일때 제일 작은 값의 인덱스찾기
-        for (int i = 0; i < arr.length; i++) {
-            if (min >= arr[i]) {
-                min = arr[i];
-                System.out.println("i = " + i);
+        for (int i = 0; i < arr.length-1; i++) {
+            // 비교기준이 되는 값을 최소값으로 설정
+            int minIdx = i;
+            // 스왑을 위한 temp
+            int temp = arr[i];
+            // 비교 대상은 인덱스 i값의 다음값을 비교할 것임
+            for (int j = i+1; j < arr.length; j++) {
+                // 반복을 통해 min을 최솟값으로 계속 재할당함
+                if (arr[minIdx]>arr[j]) {
+                    minIdx = j;
+                }
             }
+            // 최솟값을 가진 인덱스랑 비교 기준의 인덱스랑 스왑
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+            // 스왑이 잘 되고있나 디버깅
+            System.out.println("i=" + i + " / " + Arrays.toString(arr));
         }
     }
 }
