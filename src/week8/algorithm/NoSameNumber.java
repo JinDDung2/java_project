d41ef5d10fc8b01a618bf7c78c926b19979b7667
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-10 10:28:27 +0900

    [Update] solutionV2 stack.add() --> stack.push()

--- src/week8/algorithm/NoSameNumber.java
+++ src/week8/algorithm/NoSameNumber.java
@@ -29,7 +29,7 @@ public class NoSameNumber {
 
     public int[] solutionV2(int[] arr) {
         Stack<Integer> stack = new Stack<>();
-        stack.add(arr[0]);
+        stack.push(arr[0]);
 
         for (int i = 1; i < arr.length; i++) {
             if (stack.peek() != arr[i]) {
