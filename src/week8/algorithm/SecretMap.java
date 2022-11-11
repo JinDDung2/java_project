5b4de1da26920f0fca0230697a0d45872400d257
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-11 09:11:45 +0900

    [add] solve 주석

---
+++ src/week8/algorithm/SecretMap.java
@@ -0,0 +1,11 @@
+package week8.algorithm;
+
+public class SecretMap {
+    // 1. n개의 배열을 만든다.
+    // 2. arr1, arr2의 값을 이진수(binary)로 바꾼 새로운 배열 2개를 만든다.
+    // 3. for 문을 돌아 이진수로 바뀐 값이 담겨져 있는 배열 2개 중 1을 "#"으로, 0은 " "으로 채운다.
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}
