b47661547ab16fdde3f1872d5ed0c09b791d0a5d
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-14 10:13:28 +0900

    [add] 에라토스테네스의 체

---
+++ src/wwek9/algorithm/Eratosthenes.java
@@ -0,0 +1,23 @@
+package wwek9.algorithm;
+
+import java.util.Arrays;
+
+public class Eratosthenes {
+    public static void main(String[] args) {
+        final int N = 50;
+        int[] arr = new int[N-1];
+        System.out.println("arr.length = " + arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 2;
+        }
+        System.out.println("[ago]arr = " + Arrays.toString(arr));
+
+        for (int i = 2; i*i <= N; i++) {
+            for (int j = 2; j * i <= N; j++) {
+                arr[i * j - 2] = 0;
+            }
+        }
+        System.out.println("[after]arr = " + Arrays.toString(arr));
+    }
+}
