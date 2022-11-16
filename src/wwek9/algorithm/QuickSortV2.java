4565ecd290a511316c185ff8b1a60929b251c54b
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-16 09:37:24 +0900

    [Add] leftIdx 증가 및 rightIdx 감소

---
+++ src/wwek9/algorithm/QuickSortV2.java
@@ -0,0 +1,32 @@
+package wwek9.algorithm;
+
+import java.util.Arrays;
+
+public class QuickSortV2 {
+    public static int[] sort(int[] arr) {
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {20, 18, 5, 19, 25, 5, 50, 40};
+        int leftIdx = 0;
+        int rightIdx = arr.length - 1;
+        int pivot = arr[arr.length / 2];
+        System.out.println("pivot = " + pivot);
+
+        while (leftIdx <= rightIdx) {
+            // left 증가
+            if (arr[leftIdx] < pivot) leftIdx++;
+            // right 감소
+            if (arr[rightIdx] > pivot) rightIdx--;
+            System.out.println("leftIdx = " + leftIdx);
+            System.out.println("rightIdx = " + rightIdx);
+            /*if (leftIdx <= rightIdx) {
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+            }*/
+        }
+    }
+}
