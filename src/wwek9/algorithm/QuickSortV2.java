c3e17afa6e9a24d6b4ccf93000765946c4cc6dc3
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-17 09:38:30 +0900

    [Feat] 두 그룹으로 나누기, sort 기능 추가 but StackOverflowError 발생

--- src/wwek9/algorithm/QuickSortV2.java
+++ src/wwek9/algorithm/QuickSortV2.java
@@ -3,16 +3,42 @@ package wwek9.algorithm;
 import java.util.Arrays;
 
 public class QuickSortV2 {
-    public static int[] sort(int[] arr) {
+    public static void sort(int[] arr, int startIdx, int endIdx) {
+        if (startIdx >= endIdx) return;
 
-        return arr;
+        int pivot = divideTwoGroup(arr, startIdx, endIdx);
+        sort(arr, startIdx, pivot - 1);
+        sort(arr, pivot, endIdx);
+    }
+
+    public static int divideTwoGroup(int[] arr, int leftIdx, int rightIdx) {
+        int pivot = arr[arr.length / 2];
+
+        while (leftIdx <= rightIdx) {
+            // left 증가
+            while (arr[leftIdx] < pivot) leftIdx++;
+            // right 감소
+            while (arr[rightIdx] > pivot) rightIdx--;
+
+            swap(arr, leftIdx, rightIdx);
+            leftIdx++;
+            rightIdx--;
+
+        }
+        return leftIdx;
+    }
+
+    private static void swap(int[] arr, int leftIdx, int rightIdx) {
+        int temp = arr[leftIdx];
+        arr[leftIdx] = arr[rightIdx];
+        arr[rightIdx] = temp;
     }
 
     public static void main(String[] args) {
         int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
         int leftIdx = 0;
         int rightIdx = arr.length - 1;
-        int pivot = arr[arr.length / 2];
+        /*int pivot = arr[arr.length / 2];
         System.out.println("pivot = " + pivot);
 
         while (leftIdx <= rightIdx) {
@@ -30,6 +56,8 @@ public class QuickSortV2 {
                 rightIdx--;
             }
         }
+        System.out.println("arr = " + Arrays.toString(arr));*/
+        sort(arr, leftIdx, rightIdx);
         System.out.println("arr = " + Arrays.toString(arr));
     }
 }
