3074861de0272afaa37b8823bf753be79ea7926c
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-22 09:44:11 +0900

    [Add] array 있는 값 모두 더하기(재귀)

--- src/week10/algorithm/Recurrence.java
+++ src/week10/algorithm/Recurrence.java
@@ -9,8 +9,20 @@ public class Recurrence {
 
     }
 
+    public void printArraySum(int[] arr, int idx, int sum) {
+        if (idx == arr.length) {
+            System.out.println(sum);
+            return;
+        }
+        sum += arr[idx];
+        idx++;
+        printArraySum(arr, idx, sum);
+    }
+
     public static void main(String[] args) {
         Recurrence sol = new Recurrence();
-        sol.print100(1);
+//        sol.print100(1);
+        int[] arr = {7, 3, 2, 9};
+        sol.printArraySum(arr, 0, 0);
     }
 }
