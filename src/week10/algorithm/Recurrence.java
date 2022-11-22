71743777ad59d22dd67424feda580979512e3b92
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-22 09:57:42 +0900

    [Add] list 있는 값 모두 더하기(재귀)

--- src/week10/algorithm/Recurrence.java
+++ src/week10/algorithm/Recurrence.java
@@ -1,5 +1,8 @@
 package week10.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recurrence {
     public void print100(int num) {
         if (num > 100) return;
@@ -19,10 +22,26 @@ public class Recurrence {
         printArraySum(arr, idx, sum);
     }
 
+    public void printListSum(List<Integer> list, int idx, int sum) {
+        if (idx == list.size()) {
+            System.out.println(sum);
+            return;
+        }
+        sum += list.get(idx);
+        idx++;
+        printListSum(list, idx, sum);
+    }
+
     public static void main(String[] args) {
         Recurrence sol = new Recurrence();
 //        sol.print100(1);
-        int[] arr = {7, 3, 2, 9};
-        sol.printArraySum(arr, 0, 0);
+//        int[] arr = {7, 3, 2, 9};
+//        sol.printArraySum(arr, 0, 0);
+        List<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(3);
+        list.add(2);
+        list.add(9);
+        sol.printListSum(list, 0, 0);
     }
 }
