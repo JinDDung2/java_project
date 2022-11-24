62c01cacfd9b6382575c46ca861904c78cf7db1e
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-24 09:35:30 +0900

    [Add] 계단 뛰어 오르기

--- src/week10/algorithm/CodeUp.java
+++ src/week10/algorithm/CodeUp.java
@@ -9,10 +9,18 @@ public class CodeUp {
         codeUp1852(n, idx+1);
     }
 
+    public int codeUp1856(int n) {
+        if (n == 1) return 1;
+        else if (n == 2) return 1 + codeUp1856(1);
+        else if (n == 3) return 1 + codeUp1856(1) + codeUp1856(2);
+        else return codeUp1856(n - 3) + codeUp1856(n - 2) + codeUp1856(n - 1);
+    }
+
     public static void main(String[] args) {
         CodeUp codeUp = new CodeUp();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        codeUp.codeUp1852(n, 1);
+//        codeUp.codeUp1852(n, 1);
+        System.out.println(codeUp.codeUp1856(n));
     }
 }
