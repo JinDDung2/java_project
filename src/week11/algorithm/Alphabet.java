ae8c76f2d9f1fb08a68b729f0dfb5091d57361f4
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-28 09:17:32 +0900

    [Feat] AA~AZ 출력하기

--- src/week11/algorithm/Alphabet.java
+++ src/week11/algorithm/Alphabet.java
@@ -6,11 +6,22 @@ import java.util.List;
 public class Alphabet {
     public static void print() {
         List<String> list = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
         for (int i = 65; i <=90; i++) {
             char c = (char) i;
-            System.out.println(c);
-
+//            System.out.println(c);
+            list.add(String.valueOf(c));
         }
+
+        for (int i = 0; i < list.size(); i++) {
+            for (String s : list) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(list.get(i));
+                sb.append(s);
+                list2.add(String.valueOf(sb));
+            }
+        }
+        System.out.println("list2 = " + list2);
     }
 
     public static void main(String[] args) {
