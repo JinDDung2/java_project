54e4ee8bb3ab736c145f8883fd209ea3c26f34ed
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-30 09:27:59 +0900

    [Feat] LCA 이진트리 알고리즘

---
+++ src/week11/algorithm/LCA.java
@@ -0,0 +1,19 @@
+package week11.algorithm;
+
+import java.util.Scanner;
+
+public class LCA {
+    public static void distanceOfNode(int a, int b, int count) {
+//        System.out.printf("a=%d, b=%d, count=%d\n", a, b, count);
+        if (a == b) System.out.println(count);
+        else if (a > b) distanceOfNode(a / 2, b, count + 1);
+        else if (b > a) distanceOfNode(a, b / 2, count + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        distanceOfNode(num1, num2, 0);
+    }
+}
