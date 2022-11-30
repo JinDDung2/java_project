eecc27010ea3f1304306b808a3cae30b5fed49ae
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-30 09:08:07 +0900

    [Feat] Radix 정렬

--- src/week11/algorithm/NumberOfDigit.java
+++ src/week11/algorithm/NumberOfDigit.java
@@ -22,23 +22,33 @@ public class NumberOfDigit {
         return answer;
     }
 
-    public int[] sort(int[] arr) {
+    public static int[] sort(int[] arr, int digit) {
         Queue<Integer>[] queueArr = new Queue[10];
         for (int i = 0; i < queueArr.length; i++) {
             queueArr[i] = new ArrayDeque<>();
         }
 
         for (int i = 0; i < arr.length; i++) {
-            int digit = (int) (Math.pow(10, 0));
-            queueArr[Math.floorDiv(arr[i], digit) % 10].add(arr[i]);
+            int divisor = (int) (Math.pow(10, digit-1));
+            queueArr[Math.floorDiv(arr[i], divisor) % 10].add(arr[i]);
         }
 
-        return new int[10];
+        // queue에서 꺼내서 arr로 만들기
+        int idx = 0;
+        for (int i = 0; i < queueArr.length; i++) { // queueArr을 반복 합니다.
+            while(!queueArr[i].isEmpty()){
+                arr[idx++] = queueArr[i].poll();
+            }
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{0, 1, 2, 1111111111};
-        int[] rst = getDigits(arr);
-        System.out.println("rst = " + Arrays.toString(rst));
+        int[] digits = getDigits(arr);
+        for (int digit : digits) {
+            sort(arr, digit);
+        }
     }
 }
