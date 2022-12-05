b69c4eef8271e1f3c7fcafa01702e5c641aec0e7
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-05 09:53:33 +0900

    [Feat] heapQ 구현하기

--- src/week12/algorithm/HeapQ.java
+++ src/week12/algorithm/HeapQ.java
@@ -6,9 +6,10 @@ public class HeapQ {
     public static int[] heapV1() {
         int[] arr = new int[]{6, 5, 7, 8};
         for (int i = arr.length - 1; i >= 1; i--) {
-            if (i % 2 == 0) swap(arr, i, i/2-1);
-            else swap(arr, i, i/2);
+            if (i % 2 != 0 && arr[i] > arr[i/2]) swap(arr, i, i/2);
+            else if (i % 2 == 0 && arr[i] > arr[i/2-1])swap(arr, i, i/2-1);
             System.out.println("i = " + i);
+            System.out.println("arr = " + Arrays.toString(arr));
         }
         return arr;
     }
