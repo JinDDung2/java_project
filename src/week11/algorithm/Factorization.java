d146ed03af70f2d803d9c1d0ed8d795520396765
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-08 09:41:09 +0900

    [Feat] 들어오는 값이 0 이하는 -1로 처리

--- src/week11/algorithm/Factorization.java
+++ src/week11/algorithm/Factorization.java
@@ -3,21 +3,24 @@ package week11.algorithm;
 import java.util.Scanner;
 
 public class Factorization {
-    public static void intoPrime(int n) {
+    public static int intoPrime(Long n) {
         int idx = 2;
+        int max = 0;
 
         while (idx <= n) {
             if (n % idx == 0) {
                 n /= idx;
-                System.out.printf("%d ", idx);
+                max = idx;
             }
             else idx += 1;
         }
+        return max;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        intoPrime(n);
+//        int n = sc.nextInt();
+        long n = sc.nextLong();
+        System.out.println(intoPrime(n));
     }
 }
