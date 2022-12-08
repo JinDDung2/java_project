5378c6325efef45be8bf1dab22c6eff938a179c6
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-08 09:41:20 +0900

    [Feat] 들어오는 값이 0 이하는 -1로 처리

--- src/week12/algorithm/Fibo.java
+++ src/week12/algorithm/Fibo.java
@@ -2,15 +2,16 @@ package week12.algorithm;
 
 public class Fibo {
     public static int fibo(int n, int[] memo) {
-        for (int i = 0; i < memo.length; i++) {
-            if (i <= 1) memo[i] = 1;
+        if (n <= 0) return -1;
+        for (int i = 1; i < memo.length; i++) {
+            if (i <= 2) memo[i] = 1;
             else memo[i] = memo[i - 1] + memo[i - 2];
         }
         return memo[n];
     }
 
     public static void main(String[] args) {
-        int n = 7;
+        int n = 8;
         int[] memo = new int[n + 1];
         int result = fibo(n, memo);
         System.out.println("result = " + result);
