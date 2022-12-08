9488a5266df1a9a3abafd10736a1f2f176b0e405
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-08 10:46:05 +0900

    [Feat] LCS 배열 만들기

---
+++ src/week12/algorithm/LCS.java
@@ -0,0 +1,11 @@
+package week12.algorithm;
+
+public class LCS {
+    public static void main(String[] args) {
+        String str1 = "ABCDCBA";
+        String str2 = "DCABDC";
+
+        // str2이 세로, str1이 가로
+        String[][] strList = new String[str2.length()][str1.length()];
+    }
+}
