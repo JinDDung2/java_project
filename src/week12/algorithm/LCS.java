8996ddc50e8344b70596282f7598cf2551369d62
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-09 09:16:21 +0900

    [Feat] LCS 배열 루프 돌기

--- src/week12/algorithm/LCS.java
+++ src/week12/algorithm/LCS.java
@@ -7,5 +7,12 @@ public class LCS {
 
         // str2이 세로, str1이 가로
         String[][] strList = new String[str2.length()][str1.length()];
+
+        for (int i = 0; i < str2.length(); i++) {
+            for (int j = 0; j < str1.length(); j++) {
+                System.out.printf("i: %s | j: %s \n", str2.charAt(i), str1.charAt(j));
+            }
+        }
+
     }
 }
