37b0d49bd80d6bc7495d450f748f16b2a8630de5
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-09 10:01:19 +0900

    [Feat] 메모지에이션으로 배열에 값 넣기

--- src/week12/algorithm/LCS.java
+++ src/week12/algorithm/LCS.java
@@ -1,5 +1,7 @@
 package week12.algorithm;
 
+import java.util.Arrays;
+
 public class LCS {
     public static void main(String[] args) {
         String str1 = "ABCDCBA";
@@ -7,12 +9,20 @@ public class LCS {
 
         // str2이 세로, str1이 가로
         String[][] strList = new String[str2.length()][str1.length()];
+        int[][] memo = new int[str2.length()+1][str1.length()+1];
 
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j = 0; j < str1.length(); j++) {
-                System.out.printf("i: %s | j: %s \n", str2.charAt(i), str1.charAt(j));
+        for (int i = 1; i < str2.length()+1; i++) {
+            for (int j = 1; j < str1.length()+1; j++) {
+//                System.out.printf("i: %s | j: %s \n", str2.charAt(i), str1.charAt(j));
+                if (str2.charAt(i-1) == str1.charAt(j-1)) memo[i][j] = memo[i - 1][j - 1] + 1;
+                else {
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+                }
             }
         }
 
+        for (int[] ints : memo) {
+            System.out.println("ints = " + Arrays.toString(ints));
+        }
     }
 }
